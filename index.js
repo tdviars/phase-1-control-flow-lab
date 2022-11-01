a23c764dd63a5ede0e35324340fba68f2762719c
taylor <taylor.viars@yahoo.com>
2022-11-01 15:27:44 -0400

    commit tree 557f687ee2a9cf4f593a3e9528c3747ea57ff98c

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <= 400){
+    return 'This one is on me!'
+  } else if (ride > 400 && ride < 2000){
+      return 'That will be twenty bucks.'
+  } else if (ride > 2500){
+      return 'No can do.'
+  } else if (ride > 2000){
+      return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+  
+  
+
+
+function ternaryCheckCity(city){
+return (city === "NYC") ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(string){
+  switch(string){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    case "thanks for everything":
+      return "Bye."
+  }
 }
